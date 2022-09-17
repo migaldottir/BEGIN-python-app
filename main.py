8a8f606e7e3538f51429a9e77662e3c5f437dcf9
migaldottir <none399@outlook.com>
2022-09-17 23:33:18 +0200

    added a button - coloured it and set its size

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=600, width=600, bg="#263D42")
 canvas.pack()
 frame = tk.Frame(root, bg="blue")
-frame.place(relwidth=0.8, relheight=0.8)
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42")
+
+openFile.pack()
 
 root.mainloop()
 
